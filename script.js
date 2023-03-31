eab47c42ba78e0ca438db3bb737308689d4bbbc7
Satyam <satyam19arya@gmail.com>
2023-03-31 23:09:06 +0530

    initial changes

--- script.js
+++ script.js
@@ -29,66 +29,11 @@ const headerAnim = () => {
 
   header.classList.contains("header-scroll-style")
     ? offsetY < 50 && header.classList.remove("header-scroll-style")
-    : offsetY > 200 && header.classList.add("header-scroll-style");
+    : offsetY > 100 && header.classList.add("header-scroll-style");
 };
 
 headerAnim();
 window.addEventListener("scroll", headerAnim);
-
-//-----------------------------------------------
-//          LOADER FUNCTION
-//-----------------------------------------------
-
-slowInternet = setTimeout(() => {
-  document.querySelector(".loaderDiv p").innerHTML = "Slow internet :(";
-}, 3000);
-
-almostReady = setTimeout(() => {
-  document.querySelector(".loaderDiv p").innerHTML = "Loading...";
-}, 7000);
-
-document.onreadystatechange = () => {
-  if (document.readyState === "complete") {
-    // document ready
-    clearTimeout(slowInternet);
-    clearTimeout(almostReady);
-
-    document.querySelector(".loaderDiv p").innerHTML = "Loading...";
-
-    setTimeout(() => {
-      document.querySelector(".loaderDiv p").innerHTML = "Loading...";
-    }, 1000);
-    setTimeout(() => {
-      document.querySelector(".loaderDiv").classList.add("removeLoader");
-      document.querySelector("body").style.overflowY = "scroll";
-    }, 1500);
-
-    window.scrollTo(0, 0);
-  }
-  window.scrollTo(0, 0);
-};
-
-//-----------SKELETON LOADER--------------//
-
-const setSkeleton = () => {
-  const ImgDiv = document.querySelectorAll(`.imgDiv`);
-
-  ImgDiv.forEach((div) => {
-    var image = div.querySelector(`img`);
-
-    if (!image.src.includes("/assets/")) {
-      image.style.scale = 0;
-      image.style.transition = "all 0.2s";
-
-      image.onload = (img) => {
-        img.target.style.scale = "1";
-        div.classList.remove("skeleton-box");
-      };
-    } else {
-      div.classList.remove("skeleton-box");
-    }
-  });
-};
 
 /*--------------------------------------------------------------------
     Function for Hilighting navlinks on scroll to particular section
